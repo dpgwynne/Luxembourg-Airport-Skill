85cde11e0b89f97f001e12eac5c80c54c1de3a77
David Gwynne <dpgwynne@hotmail.com>
2017-12-28 23:26:32 +0100

    Using SSML type

--- lambda_function.py
+++ lambda_function.py
@@ -17,19 +17,8 @@ import time
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
     return {
         'outputSpeech': {
-            'type': 'PlainText',
-            'text': output
-        },
-        'card': {
-            'type': 'Simple',
-            'title': "SessionSpeechlet - " + title,
-            'content': "SessionSpeechlet - " + output
-        },
-        'reprompt': {
-            'outputSpeech': {
-                'type': 'PlainText',
-                'text': reprompt_text
-            }
+            'type': 'SSML',
+            'ssml': '<speak>' + output + '</speak>'
         },
         'shouldEndSession': should_end_session
     }
